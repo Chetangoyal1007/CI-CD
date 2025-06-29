869f795f2b24eee3878f384592ca31f17d60cd0f
Chetangoyal1007 <chetan1457.be22@chitkara.edu.in>
2025-06-29 20:38:58 +0530

    added one more number in sum

--- sum.java
+++ sum.java
@@ -2,7 +2,8 @@ public class sum {
     public static void main(String[] args) {
         int num1 =2 ;
         int num2 = 8;
-        int sum = num1 + num2;
+        int num3=5;
+        int sum = num1 + num2+ num3;;
         System.out.println("Sum: " + sum);
     }
 }
